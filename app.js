53e1654f7de2a596c91904dbe8ca0e5a3c03378f
grw <1215884602@qq.com>
2019-05-29 03:14:16 +0800

    socket.io

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var router = require('./src/router/index');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server, {origins: '*'});
+io.set("origin","");
 
 //自动注册interceptor拦截
 router(app, path.join(__dirname, 'src', 'interceptor'));
@@ -17,4 +20,13 @@ app.use((req, res, next)=> {
     global.res = res;
     next();
 });
-app.listen(8888);
+
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world1' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
+
+app.listen(8888,{origins: '*:*'});
