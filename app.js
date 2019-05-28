56079d0e2ea825a3f5a8190f27f6eb1838a9240d
grw <1215884602@qq.com>
2019-05-29 03:53:41 +0800

    socketio实现

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var router = require('./src/router/index');
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io').listen(server, {origins: '*'});
-io.set("origin","");
 
+//设置允许跨域访问该服务.
+app.all('*',function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:9527');
+    res.header('Access-Control-Allow-Headers', 'X-Token, Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Credentials','true');   // 新增
+    if (req.method == 'OPTIONS') {
+        res.send(200); /*让options请求快速返回*/
+    }
+    else {
+        next();
+    }
+});
 //自动注册interceptor拦截
 router(app, path.join(__dirname, 'src', 'interceptor'));
 //自动映射controller路由
@@ -21,12 +31,15 @@ app.use((req, res, next)=> {
     next();
 });
 
-
-io.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world1' });
-    socket.on('my other event', function (data) {
-        console.log(data);
+var server = app.listen(8888);
+var io = require('socket.io').listen(server);
+io.sockets.on('connection', (socket) => {
+    //发送消息(所有用户)
+    socket.broadcast.emit('message',{text: '你的好某XXX上线了'});
+    //发送消息(单个用户)
+    socket.emit('message',{text:'你上线了'});
+    //断开连接
+    socket.on('disconnect',function(){
+        console.log('User disconnected');
     });
 });
-
-app.listen(8888,{origins: '*:*'});
